cd92e7486d58d07d80879012ff092114d6afedbe
Marisha (laptop) <aethereal@gmail.com>
2017-10-21 23:10:07 -0700

    Changed the playing field to a two-dimensional array.

--- Hitoya/PlayingField.cs
+++ Hitoya/PlayingField.cs
@@ -15,35 +15,47 @@ namespace Hitoya
     {
 
         public enum rotation { xpos, xneg, ypos, yneg }
-        public static BattleTile A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P;
-        public static BattleTile[] Field = new BattleTile[] { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P };
+        //public static BattleTile A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P;
+        //public static BattleTile[] Field = new BattleTile[] { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P };
+        public static Slot[,] Field = new Slot[4,4];
+
 
         static PlayingField()
         {
 
-            for (int i = 0; i < Field.Length; i++)
+            for (int i = 0; i < Field.GetLength(0); i++)
             {
-                Field[i] = null;
+                for (int j = 0; j < Field.GetLength(0); j++)
+                {
+                    Field[i, j] = null;
+                }
             }
         }
 
         /// <summary>
-        /// A slot bears a label, and may contain a Battle Tile. A Battle Tile must be assigned with a North Direction
+        /// Places a battle tile in a slot.
+        /// </summary>
+        public static void PlaceTile(BattleTile tile, int x, int y)
+        {
+            Field[x, y].PlacedTile = tile;
+        }
+
+        /// <summary>
+        /// A slot may contain a Battle Tile. A Battle Tile must be assigned with a North Direction
         /// </summary>
         public class Slot
         {
             BattleTile placed;
             rotation nDir;
-            char name;
 
             public BattleTile PlacedTile
-            { get { return placed; } }
+            { get { return placed; }
+                set { placed = value; }
+            }
 
             public rotation NorthDirection
             { get { return nDir; } }
 
-            public Char Name
-            { get { return name; } }
 
         }
 
